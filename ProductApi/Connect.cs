aea5788d2b0e37e2ec475fb99f4e4c2a95d2a056
SzBalesz <szarazb@kkszki.hu>
2024-10-01 14:19:16 +0200

    Connect class is ready to use

---
+++ ProductApi/Connect.cs
@@ -0,0 +1,30 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductApi
+{
+    public class Connect
+    {
+        public MySqlConnection Connection { get; set; }
+        public string ConnectionString { get; set; }
+        private string Host;
+        private string Database;
+        private string User;
+        private string Password;
+
+        public Connect()
+        {
+            Host = "localhost";
+            Database = "shop";
+            User = "root";
+            Password = "";
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + User + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+    }
+}
