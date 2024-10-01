f7e0d58c5236130e12cbfe26dc6649302e26a252
SzBalesz <szarazb@kkszki.hu>
2024-10-01 14:43:21 +0200

    Getting products from database is working

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f769957e5e8c3e7d17ff8d5f16e3d9d934f9cd1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea5788d2b0e37e2ec475fb99f4e4c2a95d2a056")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
