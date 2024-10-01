f769957e5e8c3e7d17ff8d5f16e3d9d934f9cd1a
SzBalesz <szarazb@kkszki.hu>
2024-10-01 13:55:54 +0200

    Sql file with shop database.

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f92a1e032994425f4eee8d5a78b8d5f10901e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
